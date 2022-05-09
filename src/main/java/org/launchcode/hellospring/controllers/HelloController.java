dbafa97fd684bfaf7913d235e69b32e8953b64d9
Kieran <kieranbarry51@gmail.com>
2022-05-08 20:02:49 -0500

    hello-spring starter code

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,46 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    //handles request of form /hello?name=launchCode;
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handles request of form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + // submit request to /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet Me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
